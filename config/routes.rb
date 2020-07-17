7f0733b2dcb9037d11da3143d4435bf5ddcaff13
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-17 12:37:53 +0100

    add bootstrap to style page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :accounts
+  root to: "public#homepage"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

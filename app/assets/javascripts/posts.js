3d78033e4ccd9d05a996b055cbb8f41c03043e7c
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-21 16:23:56 +0100

    modify variable name for jQuery

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -6,9 +6,9 @@ $(function() {
 
 
   $(".post-like").on("click", function() {
-    let post_id = $(this).data("id");
+    let postId = $(this).data("id");
     $.ajax({
-      url: "/post/like/"+post_id,
+      url: "/post/like/"+postId,
       method: "GET"
     }).done(function(response) {
       console.log(response);

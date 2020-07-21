80b2e5d41dfcc73bb980836307e4f4e22c7c0fec
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-21 15:29:48 +0100

    Refactor view for forms

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,8 +1,14 @@
 module AccountsHelper
-  def profile_picture(user, width = 50)
+  def profile_picture(user, width = 50, height = 50)
     default_img = 'https://res.cloudinary.com/oa7/image/upload/v1595283275/default_profile_pics/download_vg4tp3.png'
     image_path = user.image.present? ? user.image.url : default_img
-    image_tag(image_path, width: width, class: 'profile-pic') 
+    image_tag(image_path, width: width, height: height, class: 'profile-pic') 
+  end
+
+  def side_picture(user, width = 50, height = 50)
+    default_img = 'https://res.cloudinary.com/oa7/image/upload/v1595283275/default_profile_pics/download_vg4tp3.png'
+    image_path = user.image.present? ? user.image.url : default_img
+    image_tag(image_path, width: width, height: height, class: 'profile-pic float-left mr-2') 
   end
 
   def actual_user?(account_id)
